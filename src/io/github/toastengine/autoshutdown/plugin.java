9afaeff3522b3900e9078779fd3670f10a18be9f
ToastEngine <ToastEngine@hotmail.com>
2017-03-23 09:04:14 -0400

    Attempted command Addition

--- src/io/github/toastengine/autoshutdown/plugin.java
+++ src/io/github/toastengine/autoshutdown/plugin.java
@@ -11,6 +11,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -31,13 +33,23 @@ public class plugin extends JavaPlugin {
         ShutdownThread = new Thread(new ShutdownTask());
         ShutdownThread.start();
         // Help!?sdlkasjdlkas
-        
+
     }
 
     @Override
     public void onDisable() {
         Logger.log(Level.INFO, "[AutoShutdown]: Disabled v" + SelfDesc.getVersion());
         ShutdownThread.interrupt();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("basic")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
+            Logger.log(Level.INFO, "basic command ran :0?");
+            return true;
+        } //If this has happened the function will return true. 
+        // If this hasn't happened the value of false will be returned.
+        return false;
     }
 
 }
